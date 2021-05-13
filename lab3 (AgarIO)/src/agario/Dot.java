8fe41bb40548514f4c92b4a48c39a161f64b0219
Dmitriy <58227907+Ta3ikNSU@users.noreply.github.com>
2021-05-13 14:09:45 +0700

    add game

---
+++ lab3 (AgarIO)/src/agario/Dot.java
@@ -0,0 +1,22 @@
+package agario;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Dot extends Entity {
+
+    Dot(int x, int y, int size) {
+        super(x, y, size);
+        Random rand = new Random();
+        int red = rand.nextInt(255);
+        int green = rand.nextInt(255);
+        int blue = rand.nextInt(255);
+        this.color = new Color(red, green, blue);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x, y, size, size);
+    }
+}
---
+++ lab3 (AgarIO)/src/agario/Entity.java
@@ -0,0 +1,44 @@
+package agario;
+
+import java.awt.*;
+
+public abstract class Entity {
+    protected int x;
+    protected int y;
+    protected int size;
+    protected Color color;
+
+    public int getX(){
+        return x;
+    }
+    public void setX(int newX){
+        x = newX;
+    }
+    public int getY(){
+        return y;
+    }
+    public void setY(int newY){
+        y = newY;
+    }
+    public int getSizePoint(){
+        return this.size;
+    }
+    public void setSize(int newSize){
+        this.size = newSize;
+    }
+
+    protected abstract void paint(Graphics g);
+
+    protected void cordRedactor(){
+        if (this.x < 0) this.x = this.size / 2;
+        if (this.y < 38) this.y = 38 + this.size / 2;
+        if (this.x + this.size > 895)  this.x = (895 - this.size);
+        if (this.y + this.size > 895) this.y = (895 - this.size);
+    }
+
+    protected Entity(int x, int y, int size){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+}
